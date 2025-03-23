5a594fbe24ea7102b6a70c55d6f897d7ce3fb320
HamzaARS2 <hamza1arrastec@gmail.com>
2025-03-23 19:38:08 +0000

    fixed problem in cpp_module01/ex04 in main

--- cpp_module_01/ex04/main.cpp
+++ cpp_module_01/ex04/main.cpp
@@ -1,9 +1,10 @@
 #include "IOFileManager.hpp"
 
 int	main(int ac, char *av[]) {
-	if (ac < 4) return 1;
+	if (ac < 4) 
+		return 1;
 
-	InputFile inputFile = InputFile(av[1]);
+	InputFile inputFile(av[1]);
 	IOFileManager fileManager = IOFileManager(inputFile);
 
 	OutputFile* outFile = fileManager.replaceFileData(av[2], av[3]);
