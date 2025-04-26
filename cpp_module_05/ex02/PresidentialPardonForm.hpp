8cb41020d12460245ff144222fa0c515e6bc7be9
HamzaARS2 <hamza1arrastec@gmail.com>
2025-04-26 18:23:07 +0100

    module_05 ex02 PresidentialPardonForm done ?

--- cpp_module_05/ex02/PresidentialPardonForm.hpp
+++ cpp_module_05/ex02/PresidentialPardonForm.hpp
@@ -16,7 +16,7 @@ class PresidentialPardonForm: public AForm {
 		PresidentialPardonForm& operator=(const PresidentialPardonForm& other);
 		
 		// overridden function
-		void	execute(Bureaucrat const& executor) const;
+		void	action() const;
 
 		~PresidentialPardonForm();
 };
--- cpp_module_05/ex02/main.cpp
+++ cpp_module_05/ex02/main.cpp
@@ -1,10 +1,11 @@
 #include "AForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 int	main() {
 	srand(time(0));
-	RobotomyRequestForm form;
+	PresidentialPardonForm form;
 	Bureaucrat crat("Fennec", 4);
 	form.beSigned(crat);
 	form.execute(crat);
